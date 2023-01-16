ad1f8b7ec4f9a9821a0054cc1b0564ec7fdd2149
João Pedro Favarato <98183878+docafavarato@users.noreply.github.com>
2023-01-16 17:34:17 -0300

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -3,30 +3,28 @@ import requests
 class Pokemon:
     def __init__(self, url):
         self.url = url
+        self.req = requests.get(self.url)
+        self.data = self.req.json()
 
     def name(self):
-        req = requests.get(self.url)
-        data = req.json()
-        pokemon_name = data['forms'][0]['name']
+        pokemon_name = self.data['forms'][0]['name']
         return pokemon_name
     
     def sprite(self):
-        req = requests.get(self.url)
-        data = req.json()
-        pokemon_sprite = data['sprites']['other']['official-artwork']['front_default']
+        pokemon_sprite = self.data['sprites']['other']['official-artwork']['front_default']
         return pokemon_sprite
 
     def shiny_sprite(self):
-        req = requests.get(self.url)
-        data = req.json()
-        pokemon_sprite = data['sprites']['other']['official-artwork']['front_shiny']
+        pokemon_sprite = self.data['sprites']['other']['official-artwork']['front_shiny']
         return pokemon_sprite
 
     def abilities(self):
-        req = requests.get(self.url)
-        data = req.json()
-        pokemon_abilities = data['abilities']
+        pokemon_abilities = self.data['abilities']
         abilities_list = []
         for i in range(len(pokemon_abilities)):
             abilities_list.append(pokemon_abilities[i]['ability']['name'])
         return abilities_list
+
+    def type(self):
+        pokemon_type = self.data['types'][0]['type']['name']
+        return pokemon_type
